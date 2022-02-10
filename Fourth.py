cb0c7bf43dee00a9bc8a95299ed9086f6754e2a8
vlreddy123 <99265985+vlreddy123@users.noreply.github.com>
2022-02-10 19:42:38 +0530

    Create Fourth.py

---
+++ Fourth.py
@@ -0,0 +1,12 @@
+
+num = float(input("Enter a number: "))
+if num > 0:
+   print("Positive number")
+elif num==0:
+   print("Zero")
+else:
+   print("Negative number")
+
+Output:
+Enter a number:10
+Positive number
